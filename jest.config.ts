6dd3e3f95725ca46e052c6312bcd6cc690e484c7
Dmitry Shatokhin <dmtrshatokhin@gmail.com>
2023-07-22 00:19:09 +0400

    chore: add test coverage report generation

--- jest.config.ts
+++ jest.config.ts
@@ -11,5 +11,22 @@ const config: Config.InitialOptions = {
     },
     testEnvironment: "jsdom",
     preset: "ts-jest",
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.{js,jsx,ts,tsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**",
+        "!**/package/**",
+        "!**/coverage/**",
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+    coverageReporters: ["json", "lcov", "text", "clover"],
 };
 export default config;
